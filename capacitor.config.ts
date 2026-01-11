e9862626089ac4d952376adc26dbee3f87708dee
Atheer Azaizeh <96442559+AtheerAzaizeh@users.noreply.github.com>
2026-01-11 06:47:18 +0200

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.2fb2840e91494541a9c5ea0cfe73bbd9',
+  appName: 'BARBERSHOP',
+  webDir: 'dist',
+  server: {
+    url: 'https://2fb2840e-9149-4541-a9c5-ea0cfe73bbd9.lovableproject.com?forceHideBadge=true',
+    cleartext: true
+  }
+};
+
+export default config;
